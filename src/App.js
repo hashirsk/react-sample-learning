e80927afe0189327cc91419972f9e0543c59178e
hashir <hashir.s@ogenie.app>
2020-10-01 01:17:09 +0530

    Added some package and change the link

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <h6>Ask question from us by following option</h6>
+      <a href="http://m.me/GenietalkSample">Open Messenger</a>
+      <a href="http://t.me/GenietalkSamplebot">Open Telegram</a>
+      <button>Refresh</button>
+      <li>data</li>
     </div>
   );
 }
