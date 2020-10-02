026421d4405d1d46c968a5a021d3dfd30b7dd0cb
hashir <hashir.s@ogenie.app>
2020-10-02 12:52:18 +0530

    asd

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const app = express()
 const path = require('path')
 const port = process.env.PORT || 3001
 
-app.use("/", express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(port, ()=>console.log('Listening the port', port))
